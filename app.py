bff83ac6f0f15f0d2bd29ecd53265e542af0c67b
Paul <pgwar2@gmail.com>
2025-02-23 21:59:36 -0700

    move loading to header

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def search():
     if card_id:
         # Process the search with the given commander_name
         
-        return render_template('error.html',redirect=f"/card/{card_id[0]}", message="Loading")
+        #return render_template('error.html',redirect=f"/card/{card_id[0]}", message="Loading")
         return redirect(f"/card/{card_id[0]}")
     return render_template('error.html', message=f"No results found for {card}")
 
